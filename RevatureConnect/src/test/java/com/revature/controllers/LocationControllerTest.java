441c17e3a1220444d3493b2816b7102f76481f1e
Rodrigo <rflores456123@gmail.com>
2021-04-27 10:07:45 -0700

    Create Location Test

--- RevatureConnect/src/test/java/com/revature/controllers/LocationControllerTest.java
+++ RevatureConnect/src/test/java/com/revature/controllers/LocationControllerTest.java
@@ -11,6 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import com.revature.models.Location;
 import com.revature.service.LocationService;
@@ -24,7 +28,6 @@ public class LocationControllerTest {
 
 	@Mock
 	private LocationService locationService;
-	
 
 //	@Test
 //	void deleteLocation() throws Exception { 
@@ -38,32 +41,42 @@ public class LocationControllerTest {
 //			andDo(print()); 
 //	}
 
-
-    @Test
+	@Test
 	void testGetAllLocations() throws Exception {
 		List<Location> locations = new ArrayList<>();
 		Location location1 = new Location(1, "Houston, Texas");
 		Location location2 = new Location(2, "Jackson, New Jersey");
-		
+
 		locations.add(location1);
 		locations.add(location2);
-		
+
 		when(locationService.findAll()).thenReturn(locations);
 
-		//when
+		// when
 		List<Location> resultLocations = locationController.getAllLocations();
-		
-		//then
+
+		// then
 		assertThat(resultLocations.size()).isEqualTo(2);
-		
+
 		assertThat(resultLocations.get(0)).isEqualTo(location1);
 		assertThat(resultLocations.get(1)).isEqualTo(location2);
 	}
-	
+
 	@Test
 	void createLocation() throws Exception {
-		Location location = new Location(5, "Houston, Texas");
-		
+
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+		Location locationToReturn = new Location(1, "Virginia");
+		Location locationToSave = new Location("Virginia");
+	
+		when(locationService.save(locationToSave)).thenReturn(locationToReturn);
+
+		ResponseEntity<Object> responseEntity = locationController.createNewLocation("Virginia");
+
+		assertThat(responseEntity.getStatusCodeValue()).isEqualTo(201);
+		assertThat(responseEntity.getHeaders().getLocation().getPath()).isEqualTo("/1");
+
 	}
 //
 //	private String toJson(Location location) throws JsonProcessingException {
