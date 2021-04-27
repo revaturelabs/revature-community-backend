e89c111871585d5274e01effd7507b2b0a692ebd
David Carrillo <carrillo.dact13@gmail.com>
2021-04-27 08:44:44 -0700

    add new service layer for addPost feature

---
+++ RevatureConnect/src/main/java/com/revature/service/PostsService.java
@@ -0,0 +1,34 @@
+package com.revature.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.revature.models.Posts;
+import com.revature.repositories.PostsRepository;
+
+@Service
+public class PostsService {
+
+	@Autowired
+	PostsRepository postsRepository;
+
+	public List<Posts> getAllPost() {
+
+		List<Posts> postsList = postsRepository.findAll();
+
+		return postsList;
+
+	}
+
+	public Posts addPost(Posts post) {
+
+		Posts newPost = postsRepository.save(post);
+
+		return newPost;
+
+	}
+
+}
