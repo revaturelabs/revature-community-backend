214aa5b11d72fa61a4e53dd7f5eabce53b97f52b
Tevin Gray <tevingray@Tevins-MacBook-Pro.local>
2021-04-30 14:49:09 -0700

    committing changes

--- RevatureConnect/src/main/java/com/revature/models/Response.java
+++ RevatureConnect/src/main/java/com/revature/models/Response.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
