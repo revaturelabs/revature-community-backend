d4b183b04e1ccb8888003c710ae989c6a13d02c9
Max Nicolai <maxvnicolai@gmail.com>
2021-05-06 09:00:27 -0700

    Update PostsController.java

--- src/main/java/com/revature/controllers/PostsController.java
+++ src/main/java/com/revature/controllers/PostsController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,7 +46,5 @@ public class PostsController {
    	public ResponseEntity<List<Posts>> getPostsByLocationId(@PathVariable("locationId") int locationId) {
        		List<Posts> postsByLocation = postsService.getPostsByLocationId(locationId);
         	return new ResponseEntity<>(postsByLocation, HttpStatus.OK);
-   	}
-	
-
+   	}	
 }
