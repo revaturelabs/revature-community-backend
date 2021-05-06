e639ba4b3e364c5ba9f03568a0c3335a38eac2b7
Rod97 <53624506+Rod97@users.noreply.github.com>
2021-05-05 19:33:02 -0700

    Update RevatureConnectApplication.java

--- src/main/java/com/revature/RevatureConnectApplication.java
+++ src/main/java/com/revature/RevatureConnectApplication.java
@@ -9,6 +9,7 @@ public class RevatureConnectApplication {
 	public static void main(String[] args) {
 		System.out.println("This is our Revature Connect App");
 		SpringApplication.run(RevatureConnectApplication.class, args);
+		System.out.println("This is after the app");
 	}
 
 }
