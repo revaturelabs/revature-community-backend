dcc0b5ec7201dfb5908c64908f5798ff447e87d8
Max Nicolai <maxvnicolai@gmail.com>
2021-05-06 08:59:03 -0700

    Updated post repository

--- src/main/java/com/revature/repositories/PostsRepository.java
+++ src/main/java/com/revature/repositories/PostsRepository.java
@@ -1,9 +1,14 @@
 package com.revature.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.revature.models.Posts;
 
 public interface PostsRepository extends JpaRepository<Posts, Integer> {
-
+	@Query(value = "SELECT * FROM post WHERE post.location_id = :locationId", nativeQuery = true)
+    List<Posts> getPostsByLocationId(@Param("locationId") int locationId);
 }
